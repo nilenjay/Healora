aa786dc034251819fe886113e8e6db398474c41f
nilenjay <nilenjaysinghsengar2006@gmail.com>
2026-06-07 13:18:09 +0530

    Added basic app theme

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFF2563EB);
+
+  static const secondary = Color(0xFF14B8A6);
+
+  static const background = Color(0xFFF8FAFC);
+
+  static const surface = Color(0xFFFFFFFF);
+
+  static const textPrimary = Color(0xFF191C1E);
+
+  static const textSecondary = Color(0xFF434655);
+
+  static const border = Color(0xFFE5E7EB);
+
+  static const success = Color(0xFF14B8A6);
+
+  static const error = Color(0xFFDC2626);
+}
